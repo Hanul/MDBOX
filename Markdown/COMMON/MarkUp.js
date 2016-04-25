578cf24dcb8a6d32bad02d2b605cb06329c4db14
Hanul <hanul@hanul.me>
2016-04-25 21:52:31 +0900

    highlight 처리

--- Markdown/COMMON/MarkUp.js
+++ Markdown/COMMON/MarkUp.js
@@ -1,52 +1,72 @@
-Markdown.MarkUp = METHOD({
+Markdown.MarkUp = METHOD(function(cls) {
+	'use strict';
 	
-	run : function(md) {
-		'use strict';
-		//OPTIONAL: md
+	var
+	// is done setting
+	isDoneSetting = false;
+	
+	return {
 		
-		var
-		// html
-		html,
-		
-		// footprint regex
-		footprintRegex = /\[\^(\d)\]/g,
-		
-		// footprint match
-		footprintMatch,
-		
-		// footprint bottom regex
-		footprintBottomRegex = /\[\^(\d)\]:/g,
-		
-		// footprint bottom match
-		footprintBottomMatch;
-		
-		if (md !== undefined) {
+		run : function(md) {
+			//OPTIONAL: md
 			
-			html = __marked(md);
-				
-			while(true) {
+			var
+			// html
+			html,
 			
-				footprintBottomMatch = footprintBottomRegex.exec(html);
+			// footprint regex
+			footprintRegex = /\[\^(\d)\]/g,
+			
+			// footprint match
+			footprintMatch,
+			
+			// footprint bottom regex
+			footprintBottomRegex = /\[\^(\d)\]:/g,
+			
+			// footprint bottom match
+			footprintBottomMatch;
+			
+			if (isDoneSetting !== true) {
 				
-				if (footprintBottomMatch === TO_DELETE) {
-					break;
-				}
+				__marked.setOptions({
+					highlight : function(code) {
+						return hljs.highlightAuto(code).value;
+					}
+				});
 				
-				html = html.substring(0, footprintBottomMatch.index) + '<a href="#rfn-' + footprintBottomMatch[1] + '"><span id="fn-' + footprintBottomMatch[1] + '">[' + footprintBottomMatch[1] + ']</span></a>' + html.substring(footprintBottomMatch.index + footprintBottomMatch[0].length);
+				isDoneSetting = true;
 			}
 			
-			while(true) {
-			
-				footprintMatch = footprintRegex.exec(html);
+			if (md !== undefined) {
 				
-				if (footprintMatch === TO_DELETE) {
-					break;
+				html = __marked(md);
+					
+				while(true) {
+				
+					footprintBottomMatch = footprintBottomRegex.exec(html);
+					
+					if (footprintBottomMatch === TO_DELETE) {
+						break;
+					}
+					
+					html = html.substring(0, footprintBottomMatch.index) + '<a href="#rfn-' + footprintBottomMatch[1] + '"><span id="fn-' + footprintBottomMatch[1] + '">[' + footprintBottomMatch[1] + ']</span></a>' + html.substring(footprintBottomMatch.index + footprintBottomMatch[0].length);
 				}
 				
-				html = html.substring(0, footprintMatch.index) + '<a href="#fn-' + footprintMatch[1] + '"><sup id="rfn-' + footprintMatch[1] + '">[' + footprintMatch[1] + ']</sup></a>' + html.substring(footprintMatch.index + footprintMatch[0].length);
+				while(true) {
+				
+					footprintMatch = footprintRegex.exec(html);
+					
+					if (footprintMatch === TO_DELETE) {
+						break;
+					}
+					
+					html = html.substring(0, footprintMatch.index) + '<a href="#fn-' + footprintMatch[1] + '"><sup id="rfn-' + footprintMatch[1] + '">[' + footprintMatch[1] + ']</sup></a>' + html.substring(footprintMatch.index + footprintMatch[0].length);
+				}
+				
+				html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
 			}
+			
+			return html;
 		}
-		
-		return html;
-	}
+	};
 });
